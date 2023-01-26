6ebb0c8f2fb679dbfb812279b205f1c79b31c59a
jleesys949 <jlee.post24@gmail.com>
2023-01-25 18:19:26 -0800

    Add logger util

---
+++ index.js
@@ -0,0 +1,15 @@
+const app = require('./app');
+const http = require('http');
+const config = require('./util/config');
+const logger = require('./util/logger');
+
+const server = http.createServer(app);
+server.listen(config.PORT, (error) => {
+    if (!error) {
+        // console.log('Server running on port ', config.PORT);
+        logger.info('Server running on port ', config.PORT);
+    } else {
+        // console.log('error starting app on port ', config.PORT);
+        logger.error('error starting app on port ', config.PORT);
+    }
+})
---
+++ util/config.js
@@ -0,0 +1,4 @@
+const PORT = 3003;
+// url for db
+
+module.exports = { PORT }
